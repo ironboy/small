4c14820b5ad2898cfac2779e582c5640d7f3d867
ironboy <thomas@nodehill.com>
2021-06-01 07:52:36 +0200

    port

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
 app.use(express.static('static'));
-app.listen(3000, () => console.log('Listening on port 3000'));
+let port = process.env.PORT || 80;
+app.listen(port, () => console.log('Listening on port ' + port));
